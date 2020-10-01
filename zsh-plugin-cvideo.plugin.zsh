d02fbdf3992d51427604d58120a85becd6060aba
Aubrey Portwood <code@aubreypwd.com>
2020-10-01 13:13:47 -0600

    Add the function.

--- zsh-plugin-cvideo.plugin.zsh
+++ zsh-plugin-cvideo.plugin.zsh
@@ -1,7 +1,22 @@
 #!/bin/zsh
 
 ###
- # My Plugin
+ # Compress video.
  #
+ # @since Tuesday, November 6, 2018
  # @since 1.0.0
+ #
+ # E.g:  compress-video *.mov
  ##
+function cvideo {
+	if ! [[ -x $(command -v ffmpeg) ]]; then
+		echo "Please install ffmpeg:";
+		echo "\tHomebrew: brew install ffmpeg"
+		return
+	fi
+
+	local f=$(basename -- "$1")
+	local e="${f##*.}"
+
+	ffmpeg -i "$1" -vcodec libx264 -crf 20 -y "$1-compressed.$e"
+}
